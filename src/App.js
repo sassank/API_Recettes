42cbbf5b538f94d6c50d50103daf73123c8072bc
Sassan <sassan.khalafi@gmailcom>
2020-12-01 23:31:17 +0100

    Revert "depot du code"
    This reverts commit 0c268fa367c0da59fa97453015d63d6000493ce8.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import Recipe from './Recipe';
-import { useEffect, useState } from 'react';
 
-const App = () => {
-  const APP_ID = "201f914e";
-  const APP_KEY = "bfdbd32df9f85f63799f142c191a92c6";
-
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("canard");
-
-  useEffect( () => { 
-    getRecipes();
-}, [query]);
-
-  const getRecipes = async () => {
-    const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-      );
-    const data = await response.json();
-    setRecipes(data.hits);
-    console.log(data.hits);
-  };
-
-  const updateSearch = e => {
-    setSearch(e.target.value);
-  };
-  
-  const getSearch = e => {
-    e.preventDefault();
-    setQuery(search);
-    setSearch('');
-  }
-
-  return(
+function App() {
+  return (
     <div className="App">
-      <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} 
-        onChange={updateSearch} placeholder="Chercher un plat ou un aliment" />
-        <button className="search-button" typeof="submit">
-        Rechercher</button>
-      </form>
-      <div className="recipes">
-      {recipes.map(recipe =>(
-        <Recipe 
-        key={recipe.recipe.label}
-        title={recipe.recipe.label} 
-        calories={recipe.recipe.calories}
-        image={recipe.recipe.image} 
-        ingredients={recipe.recipe.ingredients} 
-        />
-      ))}
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
